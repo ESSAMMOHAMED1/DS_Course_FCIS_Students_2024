57f68e770392ba10c6b3a15fb72f833285e29197
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-11-11 16:47:53 -0800

    SEC 3 :sparkles:

---
+++ DS_SEC3/Singel_linked_list.cpp
@@ -0,0 +1,126 @@
+//Singel linked list
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    string name; // set data
+    int number;  // set data
+    Node *next;  // set pointer to linke nodes
+};
+
+Node *H = NULL; // set main pointer  Head
+Node *T = NULL; // set main pointer  Tail
+Node *C = NULL; // set main pointer  Current
+
+//==================================================================================
+
+// add new node to head of list
+void Add_Head(string A, int B)
+{
+    C = new Node;  // creat new node
+    C->name = A;   // set data of node
+    C->number = B; // set data of node
+    // check if list is impety
+    if (H == NULL && T == NULL)
+    {
+        H = C;             // set head = current
+        T = C;             // set tail = current
+        C->next = NULL; //  set pointer next = null
+    }
+    else
+    {
+        C -> next = H; // set current of next = head
+        H = C;       // set current = head
+    }
+}
+
+//==========================================================================================
+
+// add new node to tail
+
+void Add_Tail(string A, int B)
+{
+    C = new Node;      // create new node
+    C->name = A;       // set data of node
+    C->number = B;     // set data of node
+    C->next = NULL;    // set pointer next = null >> case it's a tail (:
+    
+    // check if list is empty
+    if (H == NULL && T == NULL)
+    {
+        H = C; // set head = current
+        T = C; // set tail = current
+    }
+    else
+    {
+        T->next = C; // pointer tail of next -> current
+        T = C;       // set tail = current
+    }
+}
+
+//==============================================================================================
+// Delete Head node
+
+void Erase_Head()
+{
+    C = H;       // set current = head
+    H = H->next; // set head =>> head of next
+    delete C;    // delete current
+}
+//==============================================================================================
+
+// delete Tail node
+
+void Erase_Tail()
+{
+    C = H; // set current = head
+
+    while (C->next != T) // Cheak If
+    {                    // Current of next
+        C = C->next;     // Equal
+    }                    // Tail
+
+    delete T; // Delete tail
+
+    T = C; //set tail = current
+
+    C->next = NULL; // set pointer => null
+}
+
+//=====================================================================================
+// print linked list
+
+void Print_Nodes(){
+C = H;
+while (C!=NULL)
+{
+    cout<<"Name : "<<C->name<<","<<"ID : "<<C->number<<endl;
+    C = C->next;
+}
+
+}
+
+
+
+
+
+
+
+
+int main()
+{
+Add_Head("ahmed",1);
+Add_Head("essam",2);
+Add_Head("mohmaed",3);
+Add_Head("gnaa",4);
+Print_Nodes();
+cout<<"====================================="<<endl;
+Add_Tail("essam",12);
+Add_Tail("eseeeem",1355);
+Print_Nodes();
+Erase_Tail();
+Erase_Head();
+cout<<"====================================="<<endl;
+Print_Nodes();
+}
