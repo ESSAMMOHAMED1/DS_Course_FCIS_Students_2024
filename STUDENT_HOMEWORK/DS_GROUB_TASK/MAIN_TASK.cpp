a4d388f742de053c01f785041a9930fb0758ffb2
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-12-10 15:05:42 -0800

    GROUP TASK :bell:

---
+++ STUDENT_HOMEWORK/DS_GROUB_TASK/MAIN_TASK.cpp
@@ -0,0 +1,32 @@
+// Simple To-Do List
+// Build a To-Do List Manager that allows users to manage tasks and prioritize them using basic data structures.
+// Add Tasks: Store tasks in a linked list with details like task name and priority.
+// View Tasks: Sort tasks by priority using Bubble Sort or any (quadratic algorithms).
+// Complete Tasks: Use a queue to mark tasks as "in progress."
+
+//=================================================================
+// Example Flow:
+//=================================================================
+
+// Add Task:
+
+// > Add Task
+// Enter Task Name: Study Algorithms
+// Enter Priority (1-5): 1
+// Task Added!
+
+//===============================================================================
+
+// View Tasks:
+
+// > View Tasks
+// Tasks (sorted by priority):
+// 1. Study Algorithms (Priority 1)
+// 2. Do Laundry (Priority 3)
+
+//==================================================================================
+
+// Complete Task:
+
+// > Complete Task
+// Task 'Study Algorithms' moved to completed!
