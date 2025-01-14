2580e125e68e4fa946cd1e1dd2272bc799decf21
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2025-01-13 17:35:26 -0800

    SEC 9 :sparkles:

---
+++ DS_SEC9/Qustions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// 1 - find the second largest element in an array
+// 2 - remove duplicates from a sorted array
+// 3 - count the number of uppercase and lowercase letters in a given string
+// 4 - Find the middle element of a linked list hint (slow = slow.next , fast = fast.next.next);
+// 5 - Merge two linked lists
+// 6 - Implement a stack using an array
+// 7 - Explain the logic behind binary search and when it can be used
+// 8 - Draw the binary search tree for the following sequence of numbers: [8, 3, 10, 1, 6, 14, 4, 7, 13]
+// 9 - Draw a Binary Tree and spicifyed the main nodes
+// 10 - draw a max heap and revers it (what we called the reversed result)
+// 11 - traversal in the pervios binary tree  with the three types of it
+// 12 - Sort the array [38, 27, 43, 3, 9, 82, 10] using merge sort.
+// Draw the recursion tree showing how the array is divided and merged step-by-step.
+// final :  Implement a Queue Using Two Stacks
+int main()
+{
+}
